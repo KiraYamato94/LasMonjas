e8e06daef1187723a82c778f9961b4e600dba521
Allul <forte_crisx@hotmail.com>
2022-06-17 08:16:47 +0200

    Add files via upload
    2.0.3

---
+++ LasMonjas/Objects/Tased.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System.Linq;
+using static LasMonjas.HudManagerStartPatch;
+using Hazel;
+using LasMonjas.Patches;
+
+namespace LasMonjas.Objects
+{
+    class Tased
+    {
+        public static List<Tased> tasers = new List<Tased>();
+        private static Sprite sprite;
+        private Color color;
+        private GameObject taser;
+        private SpriteRenderer spriteRenderer;
+
+        public static Sprite getTaserSprite() {
+            if (sprite) return sprite;
+            sprite = CustomMain.customAssets.policeParalyze.GetComponent<SpriteRenderer>().sprite;
+            return sprite;
+        }
+
+        public Tased(float taserDuration, PlayerControl player) {
+
+            this.color = new Color(1f, 1f, 1f, 1f);
+
+            taser = new GameObject("Tased");
+            taser.AddSubmergedComponent(SubmergedCompatibility.Classes.ElevatorMover);
+            Vector3 position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1f);
+            taser.transform.position = position;
+            taser.transform.localPosition = position;
+            taser.transform.SetParent(player.transform);
+
+            spriteRenderer = taser.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = getTaserSprite();
+            spriteRenderer.color = color;
+
+            taser.SetActive(true);
+            tasers.Add(this);
+
+            HudManager.Instance.StartCoroutine(Effects.Lerp(taserDuration, new Action<float>((p) => {
+
+                if (p == 1f && taser != null) {
+                    UnityEngine.Object.Destroy(taser);
+                    tasers.Remove(this);
+                }
+
+            })));
+
+        }
+    }
+}
