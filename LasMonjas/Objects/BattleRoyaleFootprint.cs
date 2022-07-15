c4d503374127db17c8a37c86cb473d8aa8a8cb74
Allul <forte_crisx@hotmail.com>
2022-07-15 21:22:03 +0200

    Add files via upload
    2.1.0

---
+++ LasMonjas/Objects/BattleRoyaleFootprint.cs
@@ -0,0 +1,82 @@
+using LasMonjas.Patches;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LasMonjas.Objects
+{
+    class BattleRoyaleFootprint {
+        private static List<BattleRoyaleFootprint> footprints = new List<BattleRoyaleFootprint>();
+        private static Sprite sprite;
+        private Color color;
+        private GameObject footprint;
+        private SpriteRenderer spriteRenderer;
+        private Vector3 position;
+
+        public static Sprite getFootprintSprite() {
+            if (sprite) return sprite;
+            sprite = Helpers.loadSpriteFromResources("LasMonjas.Images.BattleroyaleFootprint.png", 100f);
+            return sprite;
+        }
+
+        public BattleRoyaleFootprint(PlayerControl player, int whichColor) {
+
+            switch (whichColor) {
+                case 0:
+                    this.color = Palette.PlayerColors[(int)player.Data.DefaultOutfit.ColorId];
+                    break;
+                case 1:
+                    this.color = Palette.PlayerColors[8];
+                    break;
+                case 2:
+                    this.color = Palette.PlayerColors[13];
+                    break;
+                case 3:
+                    this.color = Palette.PlayerColors[15];
+                    break;
+            }
+
+            footprint = new GameObject("BattleRoyaleFootprint");
+            footprint.AddSubmergedComponent(SubmergedCompatibility.Classes.ElevatorMover);
+            if (PlayerControl.GameOptions.MapId == 5) {
+                position = new Vector3(player.transform.position.x, player.transform.position.y, -0.5f);
+            }
+            else {
+                position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 1f);
+            }
+            footprint.transform.position = position;
+            footprint.transform.localPosition = position;
+            footprint.transform.Rotate(0.0f, 0.0f, UnityEngine.Random.Range(0.0f, 360.0f));
+            footprint.transform.SetParent(player.transform.parent);
+
+            spriteRenderer = footprint.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = getFootprintSprite();
+            spriteRenderer.color = color;
+
+            footprints.Add(this);
+            footprint.SetActive(true);
+
+            if (BattleRoyale.matchType == 2) {
+                HudManager.Instance.StartCoroutine(Effects.Lerp(5, new Action<float>((p) => {
+
+                    if (p == 1f && footprint != null) {
+                        UnityEngine.Object.Destroy(footprint);
+                        footprints.Remove(this);
+                    }
+                })));
+            }
+            else {
+                HudManager.Instance.StartCoroutine(Effects.Lerp(300, new Action<float>((p) => {
+
+                    if (p == 1f && footprint != null) {
+                        UnityEngine.Object.Destroy(footprint);
+                        footprints.Remove(this);
+                    }
+                })));
+            }
+        }
+        public static void clearBattleRoyaleFootprints() {
+            footprints = new List<BattleRoyaleFootprint>();
+        }
+    }
+}
---
+++ LasMonjas/Objects/BattleRoyaleShoot.cs
@@ -0,0 +1,71 @@
+using LasMonjas.Patches;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LasMonjas.Objects
+{
+    class BattleRoyaleShoot {
+        private static List<BattleRoyaleShoot> shoots = new List<BattleRoyaleShoot>();
+        private static Sprite sprite;
+        private Color color;
+        private GameObject shoot;
+        private SpriteRenderer spriteRenderer;
+        private Vector3 position;
+
+        public static Sprite getShootSprite() {
+            if (sprite) return sprite;
+            sprite = CustomMain.customAssets.royaleShoot.GetComponent<SpriteRenderer>().sprite;
+            return sprite;
+        }
+
+        public BattleRoyaleShoot(PlayerControl player, int whichColor, float angle) {
+
+            switch (whichColor) {
+                case 0:
+                    this.color = Palette.PlayerColors[(int)player.Data.DefaultOutfit.ColorId];
+                    break;
+                case 1:
+                    this.color = Palette.PlayerColors[8];
+                    break;
+                case 2:
+                    this.color = Palette.PlayerColors[13];
+                    break;
+                case 3:
+                    this.color = Palette.PlayerColors[15];
+                    break;
+            }
+
+            shoot = new GameObject("BattleRoyaleShoot");
+            shoot.AddSubmergedComponent(SubmergedCompatibility.Classes.ElevatorMover);
+            if (PlayerControl.GameOptions.MapId == 5) {
+                position = new Vector3(player.transform.position.x, player.transform.position.y, -0.5f);
+            }
+            else {
+                position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 1f);
+            }
+            shoot.transform.position = position;
+            shoot.transform.localPosition = position;
+            shoot.transform.SetParent(player.transform.parent);
+            shoot.transform.eulerAngles = new Vector3(0f, 0f, (float)(angle * 360f / Math.PI / 2f));
+
+            spriteRenderer = shoot.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = getShootSprite();
+            spriteRenderer.color = color;
+
+            shoots.Add(this);
+            shoot.SetActive(true);
+
+            HudManager.Instance.StartCoroutine(Effects.Lerp(0.5f, new Action<float>((p) => {
+
+                if (p == 1f && shoot != null) {
+                    UnityEngine.Object.Destroy(shoot);
+                    shoots.Remove(this);
+                }
+            })));
+        }
+        public static void clearBattleRoyaleShoots() {
+            shoots = new List<BattleRoyaleShoot>();
+        }
+    }
+}
