e3318c2f3d615bd56603d7871a8bbcb92375a94b
Allul <forte_crisx@hotmail.com>
2024-03-25 10:49:05 +0100

    Add files via upload
    3.7.3

--- LasMonjas/Main.cs
+++ LasMonjas/Main.cs
@@ -27,7 +27,7 @@ namespace LasMonjas
     {
         public const string Id = "me.allul.lasmonjas";
 
-        public const string VersionString = "3.7.2";
+        public const string VersionString = "3.7.3";
 
         public static System.Version Version = System.Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
